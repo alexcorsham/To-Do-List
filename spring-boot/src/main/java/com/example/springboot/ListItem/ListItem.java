1edb35527c59e41bd65c83bdada3ab24c3340d3f
Alex Corsham <alex.corsham@vauban.io>
2024-03-26 21:59:56 +0000

    add REST

--- spring-boot/src/main/java/com/example/springboot/ListItem/ListItem.java
+++ spring-boot/src/main/java/com/example/springboot/ListItem/ListItem.java
@@ -23,4 +23,20 @@ public class ListItem {
                 "ListItem[id=%s, name='%s', description='%s']",
                 id, name, description);
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- spring-boot/src/main/java/com/example/springboot/ListItem/ListItemRepository.java
+++ spring-boot/src/main/java/com/example/springboot/ListItem/ListItemRepository.java
@@ -2,12 +2,15 @@ package com.example.springboot.ListItem;
 
 import java.util.List;
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 
+@RepositoryRestResource(collectionResourceRel = "listItems", path = "listItems")
 public interface ListItemRepository extends MongoRepository<ListItem, String> {
 
-    public ListItem findByName(String name);
+    public ListItem findByName(@Param("name") String name);
 
-    public List<ListItem> findByDescription(String description);
+    public List<ListItem> findByDescription(@Param("description") String description);
 
 }
